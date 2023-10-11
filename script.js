a563f4dc9fdca7eec12de6f70ec3f0467f074da8
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-10-12 00:28:53 +0530

    hindi and english bot are added and workig properly

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const jokesinnerHTML = document.querySelector(".jokes");
 const btn = document.querySelector(".btn");
+const optionsbox = document.querySelector(".optionsbox");
 const selected = document.getElementById("jokes");
+const language = document.getElementById("language");
 
 gettingNewJoke();
 
@@ -13,20 +15,37 @@ btn.addEventListener("click", () => {
 
 async function gettingNewJoke(categories) {
 
-    try {
-        const newJoke = await fetch(`https://api.chucknorris.io/jokes/search?query=${categories}`);
-        const jokes = await newJoke.json();
-        if (jokenumber < jokes.total) {
-            const joke = jokes.result[jokenumber].value;
-            jokesinnerHTML.textContent = joke;
-            jokenumber++;
+    if (language.value == "Hindi") {
 
-        } else {
-            jokenumber = 0;
+        optionsbox.style.display = "none";
+        const responce = await fetch("https://hindi-jokes-api.onrender.com/jokes?api_key=689cac4cdccb4f0fdb6319c47b6a");
+        const hindijokes = await responce.json();
+        jokesinnerHTML.textContent = hindijokes.jokeContent;
+
+    } else {
+
+        try {
+            const newJoke = await fetch(`https://api.chucknorris.io/jokes/search?query=${categories}`);
+            const jokes = await newJoke.json();
+
+            optionsbox.style.display = "block";
+
+            if (jokenumber < jokes.total) {
+                const joke = jokes.result[jokenumber].value;
+                jokesinnerHTML.textContent = joke;
+                jokenumber++;
+
+            } else {
+                jokenumber = 0;
+            }
+
+        } catch (error) {
+            console.log(error);
         }
-    } catch (error) {
-        console.log(error);
+
     }
+
+
 
 }
 
